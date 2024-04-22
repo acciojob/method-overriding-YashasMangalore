b99306cec894a9163160c42fcb5f1c52f6bd40ca
YashasMangalore <129035779+YashasMangalore@users.noreply.github.com>
2024-04-22 22:31:32 +0530

    4 commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,24 +1,44 @@
-package com.driver;
+//package com.driver;
+//
+//public class Main
+//{
+//    public static void main(String[] args)
+//    {
+//        B b=new B();
+//        System.out.println(b.meth());
+//    }
+//}
+//class A
+//{
+//    public String meth()
+//    {
+//        return "Invoking method from class A";
+//    }
+//}
+//class B extends A
+//{
+//    public String meth()
+//    {
+//        return "Method is overridden in Extendend class B";
+//    }
+//}
+package com.driver; // Package declaration should match directory structure
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        B b=new B();
-        System.out.println(b.meth());
+public class Main {
+    public static void main(String[] args) {
+        B obj = new B();
+        System.out.println(obj.meth());
     }
 }
-class A
-{
-    public String meth()
-    {
+
+class A {
+    String meth() {
         return "Invoking method from class A";
     }
 }
-class B extends A
-{
-    public String meth()
-    {
+
+class B extends A {
+    String meth() {
         return "Method is overridden in Extendend class B";
     }
 }
