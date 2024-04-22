c3c65c90811a8fba511b1c33bcb5dabcefc61fea
YashasMangalore <129035779+YashasMangalore@users.noreply.github.com>
2024-04-22 22:14:29 +0530

    second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,19 +5,19 @@ public class Main
     public static void main(String[] args)
     {
         B b=new B();
-        b.meth();
+        System.out.println(b.meth());
     }
 }
 class A
 {
-    public String meth()
+    String meth()
     {
         return "Invoking method from class A";
     }
 }
 class B extends A
 {
-    public String meth()
+    String meth()
     {
         return "Method is overridden in Extendend class B";
     }
