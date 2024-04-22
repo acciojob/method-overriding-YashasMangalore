165db6663c8aa8abaa332048d87344b6da703c5c
YashasMangalore <129035779+YashasMangalore@users.noreply.github.com>
2024-04-22 23:04:54 +0530

    5 commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,44 +1,48 @@
-//package com.driver;
+//// Main.java
+package com.driver;
 //
-//public class Main
-//{
-//    public static void main(String[] args)
-//    {
-//        B b=new B();
+//public class Main {
+//    public static void main(String[] args) {
+//        B b = new B();
 //        System.out.println(b.meth());
 //    }
 //}
-//class A
-//{
-//    public String meth()
-//    {
+//
+//class A {
+//    String meth() {
 //        return "Invoking method from class A";
 //    }
 //}
-//class B extends A
-//{
-//    public String meth()
-//    {
-//        return "Method is overridden in Extendend class B";
+//
+//
+//class B extends A {
+//    String meth() {
+//        return "Method is overridden in Extended class B";
 //    }
 //}
-package com.driver; // Package declaration should match directory structure
-
-public class Main {
-    public static void main(String[] args) {
-        B obj = new B();
-        System.out.println(obj.meth());
-    }
-}
-
-class A {
+// Step 1: Create a class A with a method named 'meth' returning a string
+class A
+{
     String meth() {
         return "Invoking method from class A";
     }
 }
 
+// Step 2: Create a class B which extends class A
 class B extends A {
+    // Step 4: Override method 'meth' of class A in class B
+    @Override
     String meth() {
-        return "Method is overridden in Extendend class B";
+        return "Method is overridden in Extended class B";
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Step 3: Create an object of class B
+        B obj = new B();
+
+        // Step 5: Call the overridden method from the object of class B
+        System.out.println(obj.meth());
     }
 }
